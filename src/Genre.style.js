6ee36cabbcc01ae289b5418b035aa404835df1d6
Kévin Oliva <olivakevin64@gmail.com>
2022-02-23 12:55:15 +0100

    Test update genre's style

--- src/Genre.style.js
+++ src/Genre.style.js
@@ -2,9 +2,11 @@ import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
     root: {
-        padding: '8',
-        backgroundColor: 'black',
-        color: 'white'
+        padding: 8,
+        backgroundColor: '#16a085',
+        color: 'white',
+        fontSize: '20px',
+        borderRadius: '16px',
     }
 });
 
