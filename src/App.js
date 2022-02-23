c992d1f60ff6b9692e83bbfee5df780376bf0046
Kévin Oliva <olivakevin64@gmail.com>
2022-02-23 21:27:22 +0100

    Added Redux Saga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -15,13 +13,12 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { favoritesSlice } from "./slices";
-import Home from './Home';
+import createSagaMiddleware from "redux-saga";
+import { favoritesSlice, moviesSlice } from "./slices";
+import { rootSaga } from "./sagas";
 import Header from "./Header";
+import Home from "./Home";
 import Movie from "./Movie";
-import routes from './routes';
-
-const queryClient = new QueryClient();
 
 const persistConfig = {
   key: "root",
@@ -32,40 +29,43 @@ const persistedReducer = persistReducer(
   persistConfig,
   combineReducers({
     favorites: favoritesSlice.reducer,
+    movies: moviesSlice.reducer,
   })
 );
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+    sagaMiddleware,
+  ],
 });
+
+sagaMiddleware.run(rootSaga);
 
 const persistor = persistStore(store);
 
 function App() {
-
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Header />
-            <main>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/movies/:id" element={<Movie />} />
-              </Routes>
-            </main>
-          </BrowserRouter>
-        </QueryClientProvider>
+        <BrowserRouter>
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies/:id" element={<Movie />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
-  )
+  );
 }
 
 export default App;
