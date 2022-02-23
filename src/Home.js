27771293b3cfd712c989ec2432c430605f5774d9
Kévin Oliva <olivakevin64@gmail.com>
2022-02-23 13:21:16 +0100

    Added VerticalList, MovieCard and Input components

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
-import Genre from './Genre';
+import { useState } from "react";
+import Input from "./Input";
+import VerticalList from "./VerticalList";
+import data from "./data.json";
+import useStyles from "./Home.style";
 
 function Home() {
+    const classes = useStyles();
+    const [value, setValue] = useState("");
+    const onChange = (event) => setValue(event.target.value);
+    const movies = data.movies.filter(() => true);
     return (
-    <div>
-        <h1>Webflix<span role="img" aria-label="popcorn">✌</span></h1>
-        <Genre id="878" />
-    </div>
+      <div className={classes.root}>
+        <Input value={value} onChange={onChange} />
+        <VerticalList className={classes.list} data={movies} />
+      </div>
     );
-}
-
-export default Home;
+  }
+  
+  export default Home;
