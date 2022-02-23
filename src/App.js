b231a39a95648d62ceb90d8c1faa19637c6e6cfc
Kévin Oliva <olivakevin64@gmail.com>
2022-02-23 21:03:53 +0100

    Added and configured React Query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import Home from './Home';
 import Header from "./Header";
 import Movie from "./Movie";
 import routes from './routes';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <main>
-        <Routes>
-          <Route path={routes.home} element={<Home />} />
-          <Route path="/movies/:id" element={<Movie />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies/:id" element={<Movie />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
